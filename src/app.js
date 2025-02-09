b6fddfae1490b2eb38297fa39b8459f7b58b61d3
Narvana <adhikarijagdish3@gmail.com>
2025-02-09 23:17:15 +0530

    app.js cors updated

--- src/app.js
+++ src/app.js
@@ -20,8 +20,22 @@ const multer=require('multer');
 const port = process.env.PORT || 5000;
 
 
-app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://adminroyalheritage.netlify.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
+
+app.disable('x-powered-by');
+
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
