8255f248b40eed29c510083aa4f3db6703bbf7b4
Narvana <adhikarijagdish3@gmail.com>
2025-03-08 12:49:29 +0530

    Whatsapp message service added in AddEventEnquiry

--- src/controller/EventEnquiry.Controller.js
+++ src/controller/EventEnquiry.Controller.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const axios=require('axios');
 const ApiError=require('../utils/ApiResponse/ApiError');
 const ApiSuccess=require('../utils/ApiResponse/ApiSuccess');
 const {uploadToFirebase} = require('../middleware/ImageUpload/firebaseConfig');
@@ -7,9 +8,21 @@ const eventEnquiry=require('../model/EventEnquiry.model');
 
 const AddEventEnquiry=async(req,res,next)=>{
 
-    const {EnquiryName,EnquiryContact,EnquiryEmail,EnquiryMessage,eventType,eventDate,guestNo}=req.body;
+    const {EnquiryName,EnquiryContact,EnquiryEmail,EnquiryMessage, eventType, eventDate, guestNo, watNum, watMsg}=req.body;
 
     try {
+        if(!watNum || !watMsg)
+        {
+            return next(ApiError(400,`Provide a Royal Heritage Number and Message both`));
+        } 
+        else
+        {
+            const regex = /^[1-9][0-9]{9}$/; // Changed to 
+            if (regex.test(watNum) === false) {
+                return next(ApiError(400, 'Provide a 10 digit Number for watNum'));
+            }
+        }
+
         const AddEvent=new eventEnquiry({
             EnquiryName, 
             EnquiryContact, 
@@ -23,6 +36,23 @@ const AddEventEnquiry=async(req,res,next)=>{
         data=await AddEvent.save();
         if(data)
         {
+            const sendMessage = {
+                api_key: 'API-X-332433606617278301336824731-P-API',
+                phone: `91${watNum}`,
+                message: `${watMsg}`
+                };
+            axios.post('https://phone.watverifyapi.live/send-wa-message/post', sendMessage, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            })
+            .then(response => {
+            console.log('Message sent successfully:', response.data);
+            }) 
+            .catch(error => {
+            console.error('Error sending message:', error.response ? error.response.data : error.message);
+            });
+                
             return next(ApiSuccess(200,data,'Event Enquiry Added'))
         }
         return next(ApiError(400,'Event Enquiry Not Added. Some issue might occur'))
